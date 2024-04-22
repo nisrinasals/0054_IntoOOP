4bd80549f5b70fd2b55e3c505fb4db9c474ef9a1
nisrinasals <nisrinahikmawan@gmail.com>
2024-04-22 15:28:17 +0700

    menampilkan jurusan pada kelas mahasiswa

--- IntoOOP1.cpp
+++ IntoOOP1.cpp
@@ -10,7 +10,7 @@ class Mahasiswa{
     void output(){
         cout << "Nama: " << nama << endl;
         cout << "Umur: " << umur << endl;
-
+        cout << "Jurusan: " << jurusan << endl;
     }
 }
 int main(){
